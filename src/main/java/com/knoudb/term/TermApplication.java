ed0d25fb6d32a84877fb75d4efaba9e96ab0f0aa
PhilipOh <Philip.oh@obigo.com>
2021-06-02 14:29:19 +0900

    first commit

---
+++ src/main/java/com/knoudb/term/TermApplication.java
@@ -0,0 +1,13 @@
+package com.knoudb.term;
+
+        import org.springframework.boot.SpringApplication;
+        import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TermApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TermApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/knoudb/term/TermApplicationTests.java
@@ -0,0 +1,13 @@
+package com.knoudb.term;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TermApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
